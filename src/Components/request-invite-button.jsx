e6e49ba52ee0706874670ce9bd963c4487ba402f
DrPeaTea <110958309+DrPeaTea@users.noreply.github.com>
2023-04-15 04:13:59 +1000

    the final update
    cleaned up CSS code where I could, added a modified view so less screen breaking for tablet views and the transition from desktop to mobile

--- src/Components/request-invite-button.jsx
+++
@@ -1,9 +0,0 @@
-import "./request-invite-button.css";
-
-export default function invite() {
-  return (
-    <div>
-      <button className="invite-button">Request Invite</button>
-    </div>
-  );
-}
